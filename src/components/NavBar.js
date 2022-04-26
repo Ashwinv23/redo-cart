7aa4d4a465768cb91397a03adbb6045df04aa4ab
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-04-26 13:30:10 +0530

    cartSlice

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+  const { amount } = useSelector((store) => store.cart);
   return (
     <nav>
       <div className="navbar">
         <h2>Redux Toolkit</h2>
         <div className="checkout">
           <CartIcon />
-          <p className="amount">0</p>
+          <p className="amount">{amount}</p>
         </div>
       </div>
     </nav>
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+
+const initialState = {
+  cartItems: cartItems,
+  total: 0,
+  amount: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    emptyCart: (state) => {
+      state.cartItems = [];
+    },
+  },
+});
+
+export const { emptyCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
